95c890e8520853514e9754de2f54631cc412cbc5
flick <github@0xflick.xyz>
2024-12-23 00:48:42 -0700

    dyor2

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center mb-8">
-        <h1 className="text-4xl font-bold text-yellow-600">ONE WAY ETHEREUM -> BASE $PINEOWL BRIDGE</h1>
+        <h1 className="text-4xl font-bold text-yellow-600">ONE WAY ETHEREUM {'->'} BASE $PINEOWL BRIDGE</h1>
         <p className="text-lg text-gray-600">THIS IS A ONE WAY BRIDGE. YOU CANNOT BRIDGE BACK TO ETHEREUM WITHOUT INTERACTING DIRECTLY WITH THE BASE L2 BRIDGE CONTRACTS.</p>
       </div>
       <Bridge />
